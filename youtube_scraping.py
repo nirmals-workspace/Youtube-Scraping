2225914c7520dd7dd7d10c1ccc16c3d86a41ffa3
Nirmal <nirmalkumar07781@gmail.com>
2023-05-28 20:06:24 +0530

    Update youtube_scraping.py

--- youtube_scraping.py
+++ youtube_scraping.py
@@ -735,4 +735,4 @@ elif selected_viz == 'Histogram' and viz_button:
     
 with side_bar:
     add_vertical_space(1)
-    button('nirmalkumar7781', emoji='🕮', text = 'Buy me a book', floating = False)
+    button('nirmalkumar.datageek', emoji='🕮', text = 'Buy me a book', floating = False)
